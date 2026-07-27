b1ca1eb8bf6c62c34c643a7b7f53f426f1d1a433
Luis Balan <luisbalan881@gmail.com>
2026-07-27 11:51:23 -0600

    cambio de logotipo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'gt.gob.svet.orientacion',
-  appName: 'svet-app',
+  appName: 'Guia2',
   webDir: 'www'
 };
 
